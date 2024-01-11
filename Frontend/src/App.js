4b63a421af188824b13b73a726d2514152693d7b
prabhattt17 <rohit8282gupta@gmail.com>
2024-01-11 18:34:07 +0530

    #Worksheet PAge completed

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,20 +1,15 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from './Pages/LoginPage'
-import MainPage from './MainPage';
-import AssetsPage from './Pages/AssetsPage';
+import LoginPage from "./Pages/LoginPage";
+import MainPage from "./MainPage";
+import AssetsPage from "./Pages/AssetsPage";
 
 const App = () => {
   return (
-   <BrowserRouter>
-    <Routes>
-        {/* <Route path='/' element={<LoginPage/>}/> */}
-        <Route path='/home' element={<MainPage/>}/>
-        <Route path='/asset' element={<AssetsPage/>}/>
-    </Routes>
-    <MainPage/>
-   </BrowserRouter>
-  )
-}
+    <BrowserRouter>
+      <MainPage />
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
