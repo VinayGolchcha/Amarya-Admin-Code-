7f05324cb1f7cc3b995133008a14c6290ad04403
prabhattt17 <rohit8282gupta@gmail.com>
2024-01-11 18:14:29 +0530

    #Asset Page Completed

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './Pages/LoginPage'
 import MainPage from './MainPage';
+import AssetsPage from './Pages/AssetsPage';
 
 const App = () => {
   return (
    <BrowserRouter>
-    {/* <Routes>
-        <Route path='/' element={<LoginPage/>}/>
+    <Routes>
+        {/* <Route path='/' element={<LoginPage/>}/> */}
         <Route path='/home' element={<MainPage/>}/>
-    </Routes> */}
+        <Route path='/asset' element={<AssetsPage/>}/>
+    </Routes>
     <MainPage/>
    </BrowserRouter>
   )
