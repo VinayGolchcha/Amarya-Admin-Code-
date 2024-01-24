6d19d5261c8ad488d28c3cb7d12dd309b989d5d6
iujjwal26 <ujjwal.2000upadhyay@gmail.com>
2024-01-24 14:54:48 +0530

    changes on the sticky notes

--- Frontend/src/Components/NavBar.js
+++ Frontend/src/Components/NavBar.js
@@ -13,7 +13,7 @@ import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import TextsmsIcon from '@mui/icons-material/Textsms';
-import { Avatar, Typography } from '@mui/material';
+import { Avatar, TextField, Typography } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -84,7 +84,32 @@ const NavBar = ({ handleDrawerToggle }) => {
 
     const [arrow, updateArrow] = React.useState(false);
     
+    
     const [stickeyNotes , setStickeyNotes] = React.useState(['Conduct an inventory check of all IT assets, including hardware and software licenses.', 'Perform a regular backup of important data and verify its integrity.'])
+    const [addIcon ,setAddIcon] = React.useState(false);
+    const [addTask , setAddTask] = React.useState('');
+     
+    function handleIconClick(){
+        setAddIcon(!addIcon);
+    }
+
+    function handleAddTask(event){
+        setAddTask(event.target.value);
+    }
+
+    function handleDelete(index){
+        setStickeyNotes((prevStickyNotes) => {
+            const newStickyNotes = [...prevStickyNotes];
+            newStickyNotes.splice(index, 1);
+            return newStickyNotes;
+          });
+    }
+    function handleKeyDown(e){
+        if (e.key === 'Enter') {
+            setStickeyNotes((prevStickyNotes) => [...prevStickyNotes, addTask]);
+            setAddTask('');
+        }
+    }
     const [anchorE2, setAnchorE2] = React.useState(null);
 
     const handleClick = (event) => {
@@ -98,6 +123,7 @@ const NavBar = ({ handleDrawerToggle }) => {
     }
     const [anchorEl, setAnchorEl] = React.useState(null);
     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+   
 
     const isMenuOpen = Boolean(anchorEl);
     const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
@@ -243,11 +269,16 @@ const NavBar = ({ handleDrawerToggle }) => {
                                 </Badge>
                                 <Popper id={id} open={open} anchorEl={anchorE2} sx={{marginTop : '20px' , position : 'relative'}} placement='bottom-start'>
                                     <Box sx={{bgcolor: '#FFEBEB' , position : 'relative' ,top : '25px'}}>
-                                        <Box sx={{p:1 , height:'64px' ,width : '380px' , border : '1px solid #80808057' , borderRadius : '4px' , display : 'flex' , flexWrap : 'wrap' , alignItems : 'center'}}>
-                                            <Typography sx={{display : 'flex' , justifyContent : 'space-between' , width : '100%' , fontFamily :'Lato'}}>Add your task here… <Box > <AddIcon/></Box></Typography>
+                                        <Box sx={{padding : addIcon ? '2px' : '8px', height:'64px' ,width : '380px' , border : '1px solid #80808057' , borderRadius : '4px' , display : 'flex' , flexWrap : 'wrap' , alignItems : 'center'}}>
+                                            {!addIcon ? <Typography sx={{display : 'flex' , justifyContent : 'space-between' , width : '100%' , fontFamily :'Lato'}}>Add your task here… <Box > <AddIcon onClick={handleIconClick}/></Box></Typography>
+                                            : <TextField sx={{width : '100%' , height : '100%' , '& .MuiOutlinedInput-root': {
+                                                '&.Mui-focused': {
+                                                  borderColor: 'transparent', // Set the border color to transparent when focused
+                                                },
+                                              },}} value ={addTask} onChange={handleAddTask} onKeyDown={handleKeyDown} />}
                                         </Box>
-                                        {stickeyNotes.map((item) => {return(<Box sx={{ p : 1 ,width : '380px' , border : '1px solid #80808057' ,height:'64px' , borderRadius : '4px' , display : 'flex' , flexWrap : 'wrap' , alignItems : 'center'}}>
-                                            <Typography sx={{display : 'flex' , justifyContent : 'space-between' , alignItems : 'center' , fontFamily :'Lato'}}>{item} <Box><CloseIcon/></Box></Typography>
+                                        {stickeyNotes.map((item , index) => {return(<Box key={index} sx={{ p : 1 ,width : '380px' , border : '1px solid #80808057' ,height:'64px' , borderRadius : '4px' , display : 'flex' , flexWrap : 'wrap' , alignItems : 'center'}}>
+                                            <Typography sx={{display : 'flex' , justifyContent : 'space-between' , alignItems : 'center', width : '100%' , fontFamily :'Lato'}}>{item} <Box><CloseIcon onClick = {() => handleDelete(index)}/></Box></Typography>
                                         </Box>)})}
                                     </Box>
                                 </Popper>
