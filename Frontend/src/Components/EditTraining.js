131f0a5984a2b9c1bad217895273e4d199758459
Ujjwal <ujjwal.2000upadhyay@gmail.com>
2024-06-19 21:14:40 +0530

    correction

--- Frontend/src/Components/EditTraining.js
+++ Frontend/src/Components/EditTraining.js
@@ -143,7 +143,7 @@ export default function EditTraining({
               </Grid>
               <Grid item lg={12} md={12} sm={12} xs={12}>
                 <label for="item"  style={labelStyle}>
-                  Deatails
+                  Details
                 </label>
                 <br />
                 <textarea
